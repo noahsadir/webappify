8ab9dae77f644b1a698041a7580b4c5caec14be6
noahsadir <development.noahsadir@gmail.com>
2021-03-02 11:22:41 -0500

    generalized method names

--- src/Requests.js
+++ src/Requests.js
@@ -50,10 +50,10 @@ export function JSON_RETRIEVE(jobID, args){
         //Wait for promise to be fulfilled, then return valid JSON
         return promise.json().then(data => {
           console.log(data);
-          OPTIONS_REQUEST_LISTENER(jobID,true,data);
+          REQUEST_LISTENER(jobID,true,data);
         }).catch(function(error) {
           //Notify listener that request failed with error
-          OPTIONS_REQUEST_LISTENER(jobID,false,reportError("PROMISE_BROKEN"));
+          REQUEST_LISTENER(jobID,false,reportError("PROMISE_BROKEN"));
           console.log(error);
         });
       } else {
@@ -61,21 +61,21 @@ export function JSON_RETRIEVE(jobID, args){
         return promise.text().then(text => {
           //The request worked, but the data is invalid. Should be JSON.
           //Notify listener that request failed with error
-          OPTIONS_REQUEST_LISTENER(jobID,false,reportError("NOT_JSON"));
+          REQUEST_LISTENER(jobID,false,reportError("NOT_JSON"));
 
           //This issue may happen when running in the development environment. Use @test instead.
           console.log("Note: API requests can't be done in dev env. Please use test mode using @TEST");
         }).catch(function(error) {
 
           //Notify listener that request failed with error
-          OPTIONS_REQUEST_LISTENER(jobID,false,reportError("PROMISE_BROKEN"));
+          REQUEST_LISTENER(jobID,false,reportError("PROMISE_BROKEN"));
           console.log(error);
         });
       }
     })
     .catch(function(error) {
       //Data couldn't be fetched. Probably a network or config error
-      OPTIONS_REQUEST_LISTENER(jobID,false,reportError("NO_FETCH"));
+      REQUEST_LISTENER(jobID,false,reportError("NO_FETCH"));
       console.log(error);
     });
   }
