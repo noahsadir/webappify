501b13d692fc949402feb80a7bf018d1b6d18523
noahsadir <development.noahsadir@gmail.com>
2021-07-12 14:37:20 -0400

    added dynamic paths and separate key file

--- django_webappify/django_webappify/wsgi.py
+++ django_webappify/django_webappify/wsgi.py
@@ -7,7 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+from pathlib import Path
 import os
+import time
+import traceback
+import signal
+import sys
+
+siteRoot = Path(__file__).resolve().parent.parent.parent
+
+env_file = os.path.join(siteRoot, "django_env/bin/activate_this.py")
+assert os.path.exists(env_file)
+
+# Run the activation
+#execfile(env_file, dict(__file__=env_file))
+exec(open(env_file).read())
+
+sys.path.append(os.path.join(siteRoot, 'django_webappify/'))
+sys.path.append(os.path.join(siteRoot, 'django_env/'))
 
 from django.core.wsgi import get_wsgi_application
 
